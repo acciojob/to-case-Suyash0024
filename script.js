befd29d571e9195b695f394b0d554e9ea41b113e
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-07-25 20:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let low = text.toLowerCase();
+	let up = text.toUpperCase();
+}
+console.log(low + "-" + up)
 }
 
 // DO not change the code below
